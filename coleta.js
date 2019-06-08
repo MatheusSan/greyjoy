dfe4b857270947ab724549883c3c55f0903d9a1c
lucas <borges.lucasmachado@gmail.com>
2019-06-08 20:58:23 -0300

    teste

--- coleta.js
+++ coleta.js
@@ -1,7 +1,10 @@
-$.getScript("teste.js", function() {
-    alert("Script loaded but not necessarily executed.");
- });
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  
+    // set its src to the provided URL
 
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
 
 function collectMsg(msg){
     const str = msg;
