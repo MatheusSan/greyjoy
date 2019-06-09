4b51e9c58d35738d9fa78a905359b9f717d5d082
lucas <borges.lucasmachado@gmail.com>
2019-06-09 02:14:37 -0300

    oi

--- coleta.js
+++ coleta.js
@@ -1,11 +1,8 @@
 function collectMsg(msg){
     const str = msg;
     const finalArray = str.split(" ");
-<<<<<<< HEAD
     array = compareKeyword(finalArray);
     return finalArray;
-=======
     
->>>>>>> c05d7f86c4561e4c8d879ef1b3b34d6b07475764
 }
 
