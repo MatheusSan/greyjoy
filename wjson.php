31c346183da2097fe1868861a0191de52250135b
Pablo Herzberg <pabloherzberg@gmail.com>
2019-06-09 06:26:22 -0300

    ok

--- wjson.php
+++ wjson.php
@@ -2,12 +2,12 @@
 $array = $_POST['msg'];
 
 // Loading existing data:
-$json = file_get_contents("people.json");
+$json = file_get_contents("msg.json");
 $data = json_decode($json, true);
 
 // Adding new data:
 
 $data[sizeof($data) + 1] = $array;
 // Writing modified data:
-file_put_contents('people.json', json_encode($data, JSON_FORCE_OBJECT));
+file_put_contents('msg.json', json_encode($data, JSON_FORCE_OBJECT));
 ?>
