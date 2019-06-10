19629ef57e802174f0b2ce83a842da3f70d357a1
Pablo Herzberg <pabloherzberg@gmail.com>
2019-06-10 16:03:17 -0300

    salvar na máquina

--- funcoes.js
+++ funcoes.js
@@ -1,24 +1,29 @@
-function getText(msg){          //função da gabi para converter texto para array;
+function getText(msg){                                  //função da gabi para converter texto para array;
     const str = msg;
     const finalArray = str.split(" ");
     return finalArray;   
 }
 
 function armazenar(value){
-    let array = getText(value); //função da gabi
+    let array = getText(value);                         //função da gabi
 
-    const dadosJson = localStorage.getItem('dados');//busca os dados armazenados na máquina da pessoa
-    
+    const dadosJson = localStorage.getItem('dados');    //busca os dados armazenados na máquina da pessoa 
     if(dadosJson == null){
-        const arrayJson = JSON.stringify(array);    //transforma o array num formato JSON
-        localStorage.setItem("dados", arrayJson);   //armazena na máquina da pessoa;
+        const dadosJson = JSON.stringify(array);        //transforma o array num formato JSON
+        localStorage.setItem("dados", dadosJson);       //armazena na máquina da pessoa;
     }
     else{
-        array_recuperado = JSON.parse(dadosJson);      //transforma o arquivo JSON em array;
-        array_recuperado.forEach(element => {          //passa os elementos do array_recuperado para o array
-            array.push(element); 
-        });     
-        console.log(array);                         //imprime o array no console.log()
+        array_recuperado = JSON.parse(dadosJson);       //transforma o arquivo JSON em array;
+        array_recuperado.forEach(element => {           //passa os elementos do array_recuperado para o array
+                array.push(element);
+            }
+        );
+        const novoDadoJson = JSON.stringify(array);
+        localStorage.setItem("dados", novoDadoJson);
     }
+    return console.log(array);
 }
 
+function apagar(){
+    return localStorage.clear();
+}
